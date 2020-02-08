b2107167c4e2180101fcea3f4bddfec31ced3188
Ilya Grinenko <IRuss23@yandex.ru>
2020-02-08 15:22:00 +0300

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { json } = require('micro');
-const { reply, buttons } = require('alice-renderer');
+const { reply } = require('alice-renderer');
 
-const { toLowerCase } = require('./lib/helpers');
+const { toLowerCase, getButtonsByState } = require('./lib/helpers');
 const { DataBase } = require('./lib/DataBase');
 const { chat } = require('./lib/chat');
+const { start } = require('./lib/actions');
 
 const db = new DataBase();
 
@@ -13,20 +14,22 @@ module.exports = async (req, res) => {
     const key = session.session_id;
 
     if (!db.get(key))
-        db.add(key, { state: 'START' });
-    
-    result = reply`${chat(db, key, request.original_utterance)}`
+        db.add(key, { state: start });
 
-    if (request.nlu.tokens.map(toLowerCase).includes('хватит')) {
-        result = reply.end`До свидания!`
-    }     
+    let response = request.nlu.tokens.map(toLowerCase).includes('хватит')
+        ? reply.end`До свидания!`
+        : reply`${chat(db, key, request.original_utterance)}`;
+
+    response = reply`
+        ${response}
+        ${getButtonsByState(db.get(key))}
+    `;
 
     res.end(JSON.stringify(
         {
             version,
             session,
-            response: result
-            
+            response
         }
     ));
 };
